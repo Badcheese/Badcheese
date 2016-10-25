5056e38ef6181ad9e22f82d583e977804d9cc355
Daniel Shneyder <archcorsair@gmail.com>
2016-10-25 12:45:05 -0700

    Combine HTTP and Socket server

--- server/client-socket.js
+++ server/client-socket.js
@@ -1,4 +1,5 @@
 const socket = io();
+
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { clientSays: 'this message came through socket.io' });
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
+const express = require('express');
+const path = require('path');
 const app = require('express')();
 const bodyParser = require('body-parser');
 const handler = require('./helpers/request-handler.js');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 // TODO: Will move routes to own module (routes.js)
 // const router = require('.helpers/routes.js');
 
@@ -12,28 +17,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve Static Files for React
-app.use('/', express.static(path.join(__dirname, '..', 'src')));
+// app.use('/', express.static(path.join(__dirname, '..', 'src')));
+app.use('/', express.static(path.join(__dirname)));
+
 
 // Params
-app.param('boardId', handler.getBoard);
-app.param('archiveId', handler.archiveBoard);
+// app.param('boardId', handler.getBoard);
+// app.param('archiveId', handler.archiveBoard);
 
 // Routes
 // REVIEW: Serve index.html from project root?
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  // res.sendFile(path.join(__dirname, '..', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
+
 });
 
 // Create a new board
-app.get('/board', handler.getNewBoard);
-
-// Get a board by id
-app.get('/board/:boardId', handler.getBoard);
-
-// Archive board by id
-app.post('board/:archiveId', handler.archiveBoard);
+// app.get('/board', handler.getNewBoard);
+//
+// // Get a board by id
+// app.get('/board/:boardId', handler.getBoard);
+//
+// // Archive board by id (Server to Server)
+// app.post('board/:archiveId', handler.archiveBoard);
 
 // Server Port
-app.listen(3000);
+// app.listen(3000, () => {
+//   console.log('HTTP is up!');
+// });
+server.listen(3005, () => {
+  console.log('Socket is up!');
+});
+
+io.on('connection', (socket) => {
+  socket.emit('news', { serverSays: 'This message came through socket.io' });
+  socket.on('my other event', (data) => {
+    console.log(data);
+  });
+});
+
 
 module.exports = app;
