1e06c36d9238e6ca98a8b3304fe991bd7385648a
NickLathen <nick.lathen@gmail.com>
2016-10-25 20:35:48 -0700

    Add example change to liveBoard.js

--- server/helpers/liveBoard.js
+++ server/helpers/liveBoard.js
@@ -22,3 +22,49 @@ module.exports = {
     }
   }
 };
+
+/* EXAMPLE CHANGE
+var Examplechange = {
+  
+  color: cssColor, //this is optional, if included it will change the background of the board to the provided color. Otherwhise the background will stay the same as it is.  
+  
+  shapes: { //this is optional, changes to existing shapes go here
+    0: {
+      type: 'circle', //required STRING
+      points: [{x, y}], //required NUMBERS
+      radius: 'radius', //required NUMBER
+      strokeColor: 'cssColor', //optional, default is black any valid css Color is ok
+      fillColor: 'cssColor', //optional, if included circle will be filled
+      lineWidth: 'width' //optional NUMBER, default is 1px
+    }
+  },
+
+  newShapes: [ //shapes you want to add go here.
+    {
+      type: 'line', //required STRING
+      points: [{x, y}, {x, y}, {x, y}], //required n-x-tuple array, values should be integers, each object should have 'x' and 'y' property
+      strokeColor: 'cssColor', //optional, default is black any valid css Color is ok
+      lineJoin: 'type', //optional STRING, default is 'round'
+      lineCap: 'type', //optional STRING, default is 'round'
+      lineWidth: 'width' //optional NUMBER, default is 1px
+    },
+    {
+      type: 'box', //required STRING
+      points: [{x, y}, {x, y}], //required 2-x-tuple array, values should be integers, each object should have 'x' and 'y' property
+      strokeColor: 'cssColor', //optional, default is black any valid css Color is ok
+      fillColor: 'cssColor', //optional, if included box will be filled      
+      lineJoin: 'type', //optional STRING, default is 'round'
+      lineCap: 'type', //optional STRING, default is 'round'
+      lineWidth: 'width' //optional NUMBER, default is 1px
+    },
+    { //vector type might be unnecessary, as it is just a 2 point line
+      type: 'vector', //required STRING
+      points: [{x, y}, {x, y}], //required 2-x-tuple array, values should be integers, each object should have 'x' and 'y' property
+      strokeColor: 'cssColor', //optional, default is black any valid css Color is ok
+      lineJoin: 'type', //optional STRING, default is 'round'
+      lineCap: 'type', //optional STRING, default is 'round'
+      lineWidth: 'width' //optional NUMBER, default is 1px
+    }
+  ]
+};
+*/
