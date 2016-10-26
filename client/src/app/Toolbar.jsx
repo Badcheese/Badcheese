43c1072f99e30c49fc8e55720ae7a10e25968c3f
El_Gato <albertomadueno@gmail.com>
2016-10-26 12:50:11 -0700

    integreated front end test integration still in progress

---
+++ client/src/app/Toolbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+var ToolBar = (props) => (
+  <nav className="toolbar">
+    <div className="container">
+      <ul className="toolList">
+        <li className="tools">Tools</li>
+        <li className="tools">Tools</li>
+        <li className="tools">Tools</li>
+        <li className="tools">Tools</li>
+        <li className="tools">Tools</li>
+        <li className="tools">Tools</li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default ToolBar;
