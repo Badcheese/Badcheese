5883ab539e6082d05e246e6dd45a83d942807e87
El_Gato <albertomadueno@gmail.com>
2016-10-29 17:02:06 -0700

    styled out the front end

--- client/src/app/Nav.jsx
+++ client/src/app/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 var Nav = (props) => (
   <nav className="navbar">
     <div className="col-md-6 col-md-offset-3">
-      <h1>Drawmie (From Nav)</h1>
+      <h1>Drawmie                     I Dare You</h1>
     </div>
   </nav>
 );
